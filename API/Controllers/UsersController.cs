1c089f8debe2c4b74bfa464826ba5050d2ee7550
ngzk999 <ngzk916@gmail.com>
2021-10-13 22:44:39 +0800

    Add project files.

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,49 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // controller is the placeholder where the api is accessible via api/users
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            // Dependency injection on the ctor.
+            // DB is accessible via _context.
+            _context = context;
+        }
+
+        /// <summary>
+        /// End point to get all the users.
+        /// We can also use List as the return type, but List is huge and has many
+        /// unused functions inside.
+        /// </summary>
+        /// <returns>List of users.</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        /// <summary>
+        /// End point to get specific user with his/her id.
+        /// Accessible via api/users/1.
+        /// </summary>
+        /// <param name="id">Specified user's id</param>
+        /// <returns>Specified user.</returns>
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+    }
+}
