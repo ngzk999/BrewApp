40a678130c0e157b84af6bf0fb340a3b1ac7af67
ngzk999 <ngzk916@gmail.com>
2021-11-01 23:38:50 +0800

    Setting up nav bar + 2 way binding. 2 way binding is a technique to pass data from template(html) to component or vise versa. [] = receiving data from component, () pass data to component

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,16 +5,20 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavComponent } from './nav/nav.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule, // in order to make http request in angular.
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
