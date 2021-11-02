0627929a52876bbb51f9b4b122d075169901e8b7
ngzk999 <ngzk916@gmail.com>
2021-11-02 23:40:23 +0800

    Enabled nav bar dropdown toggle from NGX-Bootstrap by introducing directives (dropdown and dropdownToggle) and structure directive (*dropdownMenu).

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -18,7 +19,8 @@ import { FormsModule } from '@angular/forms';
     AppRoutingModule,
     HttpClientModule, // in order to make http request in angular.
     BrowserAnimationsModule,
-    FormsModule
+    FormsModule,
+    BsDropdownModule.forRoot() // for navbar dropdown.
   ],
   providers: [],
   bootstrap: [AppComponent]
